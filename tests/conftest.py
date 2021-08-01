30877d98f4d1216b9e3815504f7dd19aeee42923
Edward Amor <edward.amor3@gmail.com>
2021-07-31 20:22:46 -0400

    test: add module/function isolation fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,19 @@
+import pytest
+
 pytest_plugins = ["fixtures.accounts", "fixtures.deployments"]
+
+
+@pytest.fixture(scope="module", autouse=True)
+def isolate_module(chain):
+    chain.snapshot()
+    yield
+    chain.revert()
+
+
+@pytest.fixture(autouse=True)
+def isolate_function(chain, history):
+    start = len(history)
+    yield
+    end = len(history)
+    if (undo_count := end - start) > 0:
+        chain.undo(undo_count)
