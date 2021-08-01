aebb62321561f4f0bd04daafe9c2aced083d2abd
Edward Amor <edward.amor3@gmail.com>
2021-07-31 20:19:34 -0400

    test: add deployment fixtures
    - ENS registry
    - .test tld registrar
    - node.test Node

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,1 @@
-pytest_plugins = ["fixtures.accounts"]
+pytest_plugins = ["fixtures.accounts", "fixtures.deployments"]
---
+++ tests/fixtures/deployments.py
@@ -0,0 +1,22 @@
+import pytest
+
+
+@pytest.fixture(scope="session")
+def registry(alice, pm):
+    ENSRegistry = pm("@ensdomains/ens@0.6.0").ENSRegistry
+    return ENSRegistry.deploy({"from": alice})
+
+
+@pytest.fixture(scope="session")
+def registrar(alice, pm, registry, web3):
+    FIFSRegistrar = pm("@ensdomains/ens@0.6.0").FIFSRegistrar
+    registrar = FIFSRegistrar.deploy(registry, web3.ens.namehash("test"), {"from": alice})
+    registry.setSubnodeOwner("0x0", web3.ens.labelhash("test"), registrar, {"from": alice})
+    return registrar
+
+
+@pytest.fixture(scope="session")
+def node(alice, Node, registrar, registry, web3):
+    node = Node.deploy(registry, web3.ens.namehash("node.test"), {"from": alice})
+    registrar.register(web3.ens.labelhash("node"), node, {"from": alice})
+    return node
