86af9d61ed80af72f18f546d7fdc79fd8a45bd55
Edward Amor <edward.amor3@gmail.com>
2021-07-31 20:53:24 -0400

    fix: reclaim test use setup fixture

--- tests/node/test_reclaim.py
+++ tests/node/test_reclaim.py
@@ -1,11 +1,13 @@
 import brownie
+import pytest
 from brownie import ETH_ADDRESS, convert
 
+pytestmark = pytest.mark.usefixtures("mint_bob")
 
-def test_reclaiming_subnode_ownership(alice, bob, node, registry, web3):
+
+def test_reclaiming_subnode_ownership(bob, node, registry, web3):
     bob_label = web3.ens.labelhash("bob")
     bob_node = web3.ens.namehash("bob.node.test")
-    node.register(bob_label, bob, {"from": alice})
 
     registry.setOwner(bob_node, ETH_ADDRESS, {"from": bob})
     assert registry.owner(bob_node) == ETH_ADDRESS
@@ -17,7 +19,6 @@ def test_reclaiming_subnode_ownership(alice, bob, node, registry, web3):
 def test_is_guarded(alice, bob, node, registry, web3):
     bob_label = web3.ens.labelhash("bob")
     bob_node = web3.ens.namehash("bob.node.test")
-    node.register(bob_label, bob, {"from": alice})
 
     registry.setOwner(bob_node, ETH_ADDRESS, {"from": bob})
     assert registry.owner(bob_node) == ETH_ADDRESS
